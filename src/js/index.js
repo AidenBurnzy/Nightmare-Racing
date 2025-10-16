5a2b1c0670e4bbc3b7dd56d299eb031178fb7f9c
Nicholas-AV <nicholas.auctusventures@gmail.com>
2025-10-16 08:55:31 +0000

    Remove mobile sign-in links from various service and page files

--- src/js/index.js
+++ src/js/index.js
@@ -427,14 +427,6 @@ document.addEventListener('DOMContentLoaded', function() {
                         <li><a href="src/pages/featured-cars.html">Our Projects</a></li>
                         <li><a href="src/pages/shopParts.html">Shop Parts</a></li>
                         <li><a href="#contact">Contact</a></li>
-                        <li class="mobile-sign-in">
-                            <a href="https://nmrauto.com/" target="_blank" rel="noopener noreferrer">
-                                <svg class="profile-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" fill="currentColor"/>
-                                </svg>
-                                Sign In to Shop
-                            </a>
-                        </li>
                     </ul>
 
                     <div class="mobile-menu-footer">
